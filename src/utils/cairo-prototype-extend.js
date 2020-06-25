22eafa2a4fb1283341ba7e3cc9f7cdaaf8074ce3
Rom Grk <romgrk.cc@gmail.com>
2020-06-25 01:32:55 -0400

    cairo: add roundedRectangle

--- src/utils/cairo-prototype-extend.js
+++ src/utils/cairo-prototype-extend.js
@@ -10,3 +10,22 @@ Cairo.Context.prototype.setColor = function setColor(c) {
   this.setSourceRgba(c.red, c.green, c.blue, c.alpha)
 }
 
+Cairo.Context.prototype.roundedRectangle =
+function roundedRectangle(
+  x,
+  y,
+  width,
+  height,
+  radius
+) {
+    this.moveTo(x + radius, y)
+    this.lineTo(x + width - radius, y)
+    this.curveTo(x + width, y, x + width, y, x + width, y + radius)
+    this.lineTo(x + width, y + height - radius)
+    this.curveTo(x + width, y + height, x + width, y + height, x + width - radius, y + height)
+    this.lineTo(x + radius, y + height)
+    this.curveTo(x, y + height, x, y + height, x, y + height - radius)
+    this.lineTo(x, y + radius)
+    this.curveTo(x, y, x, y, x + radius, y)
+}
+
